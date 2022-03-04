c24032dee33e2aaf5c6e590353f2f97f7e21650b
dosu199 <sudo1996@gmail.com>
2022-03-04 15:20:15 +0100

    running loca

--- public/case/main.js
+++ public/case/main.js
@@ -57,16 +57,16 @@ class CardManager {
 
         cards.style.backgroundImage = "url(' " + this.randWeapon[randoo] + "')";
 
-        if (this.randWeapon[randoo] == "images/blue1.png" || this.randWeapon[randoo] == "images/blue2.png" || this.randWeapon[randoo] == "images/blue3.png" || this.randWeapon[randoo] == "images/blue4.png" || this.randWeapon[randoo] == "images/blue5.png")
+        if (this.randWeapon[randoo] == "/case/images/blue1.png" || this.randWeapon[randoo] == "/case/images/blue2.png" || this.randWeapon[randoo] == "/case/images/blue3.png" || this.randWeapon[randoo] == "/case/images/blue4.png" || this.randWeapon[randoo] == "/case/images/blue5.png")
             cards.style.borderBottom = "thick solid  #3498DB";
 
-        else if (this.randWeapon[randoo] == "images/purple1.png" || this.randWeapon[randoo] == "images/purple2.png" || this.randWeapon[randoo] == "images/purple3.png" || this.randWeapon[randoo] == "images/purple4.png")
+        else if (this.randWeapon[randoo] == "/case/images/purple1.png" || this.randWeapon[randoo] == "/case/images/purple2.png" || this.randWeapon[randoo] == "/case/images/purple3.png" || this.randWeapon[randoo] == "/case/images/purple4.png")
             cards.style.borderBottom = "thick solid  #8E44AD";
 
-        else if (this.randWeapon[randoo] == "images/red2.png")
+        else if (this.randWeapon[randoo] == "/case/images/red2.png")
             cards.style.borderBottom = "thick solid  #E74C3C";
 
-        else if (this.randWeapon[randoo] == "images/pink1.png" || this.randWeapon[randoo] == "images/pink2.png" || this.randWeapon[randoo] == "images/pink3.png")
+        else if (this.randWeapon[randoo] == "/case/images/pink1.png" || this.randWeapon[randoo] == "/case/images/pink2.png" || this.randWeapon[randoo] == "/case/images/pink3.png")
             cards.style.borderBottom = "thick solid  #FA3FE6";
 
         parent.appendChild(cards);
@@ -166,21 +166,21 @@ window.onload = () => {
         y: 5,
         width: 150,
         height: 150,
-        red1: "images/red1.png",
-        red2: "images/red2.png",
-        purple1: "images/purple1.png",
-        purple2: "images/purple2.png",
-        purple3: "images/purple3.png",
-        purple4: "images/purple4.png",
-        pink1: "images/pink1.png",
-        pink2: "images/pink2.png",
-        pink3: "images/pink3.png",
-        blue1: "images/blue1.png",
-        blue2: "images/blue2.png",
-        blue3: "images/blue3.png",
-        blue4: "images/blue4.png",
-        blue5: "images/blue5.png",
-        knife: "images/knife.png",
+        red1: "/case/images/red1.png",
+        red2: "/case/images/red2.png",
+        purple1: "/case/images/purple1.png",
+        purple2: "/case/images/purple2.png",
+        purple3: "/case/images/purple3.png",
+        purple4: "/case/images/purple4.png",
+        pink1: "/case/images/pink1.png",
+        pink2: "/case/images/pink2.png",
+        pink3: "/case/images/pink3.png",
+        blue1: "/case/images/blue1.png",
+        blue2: "/case/images/blue2.png",
+        blue3: "/case/images/blue3.png",
+        blue4: "/case/images/blue4.png",
+        blue5: "/case/images/blue5.png",
+        knife: "/case/images/knife.png",
         rand: [],
         shift: 10
     };
